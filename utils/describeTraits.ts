7132c90e38cfdb2015254a6d62a63579927ce847
sartocrates <113783574+sartocrates@users.noreply.github.com>
2024-01-06 05:10:41 -0600

    Update describeTraits.ts

--- utils/describeTraits.ts
+++ utils/describeTraits.ts
@@ -1,211 +1,220 @@
 // •┈••✦ ❤ ✦••┈• LARMF API - PHYSICAL DESCRIPTIONS •┈••✦ ❤ ✦••┈•
 
+// •┈••✦ ❤ ✦••┈• TINYDINOS API - PHYSICAL DESCRIPTIONS •┈••✦ ❤ ✦••┈•
+
 import { Larmf, LarmfAttribute, LarmfStats } from "./larmfInterfaces";
 
 function describe_traits(larmf: Larmf): string {
-    let { stats, attributes } = larmf;
-    // type can be accessed in the attributes but i've accessed it in the stats so i can add the other stats easily if we decide to do that
-    // could be fun to add descriptions for the stats as well :)
-    let { type } = stats;
+    let { attributes } = larmf;
 
 
-    // create an attributes object for the larmf of its traits
+    // create an attributes object for the tinydinos of its traits
     let extractedAttributes: Record<string, string> = {};
     attributes.forEach((trait: LarmfAttribute) => {
         extractedAttributes[trait.trait_type] = trait.value;
     });
 
-    // define the three special larmf types; their descriptions are slightly different
-    let specialTypes = ["zombie", "ape", "alien"];
-    let isSpecialType = specialTypes.includes(type.toLowerCase());
-
     // isolating the individual traits from the extractedAttributes{}
-    let smoke: string = extractedAttributes.Smoke;
-    let mouth: string = extractedAttributes.Mouth;
-    let headphones: string = extractedAttributes.Headphones;
-    let hair: string = extractedAttributes.Hair;
-    let earrings: string = extractedAttributes.Earring;
-    let eyewear: string = extractedAttributes.Eyewear;
-    let headwear: string = extractedAttributes.Headwear;
+    let background: string = extractedAttributes.background;
+    let body: string = extractedAttributes.body;
+    let chest: string = extractedAttributes.chest;
+    let eyes: string = extractedAttributes.eyes;
+    let face: string = extractedAttributes.face;
+    let feet: string = extractedAttributes.feet;
+    let hands: string = extractedAttributes.hands;
+    let head: string = extractedAttributes.head;
+    let spikes: string = extractedAttributes.spikes;
 
 
-// selecting strings based on the larmf's traits to assemble into the description
+// selecting strings based on the tinydino's traits to assemble into the description
     let phrases = {
-        type: function (): string {
-            let specialTypeDescriptions: { [key: string]: string } = {
-                'zombie': 'with a ghastly green, zombie-like body, glowing red eyes, and a drooling mouth,',
-                'ape': 'with a fur-brown monkey body, large round monkey eyes, and a slight frown,',
-                'alien': 'with a light blue otherworldly body, square blue alien eyes, and a serious facial expression,'
+
+
+
+        background: function (): string {
+            let backgroundDescriptions: Record<string, string> = {
+                'blue': ', against a blue background',
+                'pink': ', against a pink background',
+                'gray': ', against a gray background',
+                'yellow': ', against a yellow background',
+                'green': ', against a green background',
+                'orange': ', against an orange background',
+                'light_gray': ', against a light gray background',
+                'light_brown': ', against a light brown background',
+                'purple': ', against a purple background',
+                'red_linear_gradient': ', against a red linear gradient background',
+                'purple_linear_gradient': ', against a purple linear gradient background',
+                'blue_linear_gradient': ', against a blue linear gradient background',
+                'yellow_linear_gradient': ', against a yellow linear gradient background',
+                'blue_conical_gradient': ', against a blue conical gradient background',
+                'purple_conical_gradient': ', against a purple conical gradient background',
+                'red_conical_gradient': ', against a red conical gradient background',
+                'orange_conical_gradient': ', against a orange conical gradient background',
+                'green_conical_gradient': ', against a green conical gradient background',
+                'snow_landscape': ', against a snow landscape background',
+                'day_landscape': ', against a daytime landscape background',
+                'night_landscape': ', against a nightime landscape background',
             };
 
-            if (specialTypeDescriptions[type.toLowerCase() as keyof typeof specialTypeDescriptions]) {
-                return specialTypeDescriptions[type.toLowerCase() as keyof typeof specialTypeDescriptions];
-            }
+            return backgroundDescriptions[background] || '';
+        },
 
-            let typeDescriptions: Record<string, string> = {
-                'peachy': 'with a soft peach-colored body',
-                'taffy': 'with a taffy pink body',
-                'leafy': 'with a minty green body',
-                'creamy': 'with a light tan body',
-                'chilly': 'with an icy white body',
-                'moon': 'with a pale moon-like luminescent body',
-                'sandy': 'with a warm sandy beige body',
-                'slate': 'with a light slate gray body',
-                'cinnamon': 'with a warm cinnamon-brown body',
-                'earthy': 'with an earthy rich brown spotted body',
-                'chrome': 'with a shiny, reflective chrome body',
-                'gold': 'with a treasure-like golden body',
+
+        body: function (): string {
+            let bodyDescriptions: Record<string, string> = {
+                'teal': `with a teal colored body`,
+                'lime_green': `with a lime green colored body`,
+                'gray': `with a gray colored body`,
+                'green': `with a green colored body`,
+                'yellow': `with a yellow colored body`,
+                'aqua': `with an aqua colored body`,
+                'blue': `with a blue colored body`,
+                'red': `with a red colored body`,
+                'purple': `with a purple colored body`,
+                'pink': `with a pink colored body`,
+                'purple_linear_gradient': `with a purple colored linear gradient body`,
+                'orange_linear_gradient': `with an orange colored linear gradient body`,
+                'yellow_linear_gradient': `with a yellow colored linear gradient body`,
+                'blue_linear_gradient': `with a blue colored linear gradient body`,
+                'pink_linear_gradient': `with a pink colored linear gradient body`,
+                'green_linear_gradient': `with a green colored linear gradient body`,
+                'rainbow': `with a rainbow colored body`,
+                'grayspace_linear_gradient': `with a grayspace colored linear gradient body`
             };
 
-            return typeDescriptions[type.toLowerCase()] ?? 'with a unique and indescribable color';
+            return bodyDescriptions[body] || '';
         },
 
 
 
-        smoke: function (): string {
-            let smokeDescriptions: Record<string, string> = {
-                'Doobie': ', smoking a doobie',
-                'Cig': ', smoking a cigarette',
-                'Stogie': ', smoking a stogie',
-                'Vape': ', smoking an e-cigarette',
-                'Pipe': ', smoking a pipe',
-            };
-
-            return smokeDescriptions[smoke] || '';
-        },
-
-
-        mouth: function (): string {
-            let mouthDescriptions: Record<string, string> = {
-                'Smile': `and a big smile`,
-                'Normal': `and a content facial expression`,
-                'Chiclets': `and buck front teeth`,
-                'Black_Beard': `and a black beard`,
-                'Ginger_Beard': `and a ginger beard`,
-                'Smirk': `and a smirking facial expression`,
-                'Blonde_Handle_Bars': `and a blonde handlebar moustache`,
-                'Black_Handle_Bars': `and a black handlebar moustache`,
-                'Gronk': `and a gronk expression`,
-                'Zombie': `and a drooling zombie mouth`,
-                'Fangs': `and sharp fangs`,
-                'Gold_Grill': `and a gold grill`
-            };
-
-            return mouthDescriptions[mouth] || '';
-        },
-
-
-
-        hair: function (): string {
-            let hairDescriptions: Record<string, string> = {
-                'Messy': 'with messy dishevelled black hair',
-                'Peak_Spike': 'with a black deathhawk hairstyle',
-                'Long_Black': 'with long mid-back-length black hair',
-                'Wild': 'with wild dishevelled black hair',
-                'Poofy_Ginger': 'with poofy ginger hair',
-                'Poofy_Blonde': 'with poofy blonde hair',
-                'Poofy_Black': 'with poofy black hair',
-                'Mullet': 'with a black mullet hairstyle',
-                'Long_Blonde': 'with long mid-back-length blonde hair',
-                'Swanky': 'with a black Elvis-style hairdo',
-                'Swanky_Lite': 'with a blonde Elvis-style hairdo',
-                'Rondal_McDondal': 'with clown-like big red hair',
-                'Wild_Lime': 'with big wild lime-colored hair',
-                'Wild_Candy': 'with big wild candy-colored hair',
+        eyes: function (): string {
+            let eyesDescriptions: Record<string, string> = {
+                'white': 'with white eyes',
+                'light_gray': 'with light gray eyes',
+                'purple': 'with purple eyes',
+                'yellow': 'with yellow eyes',
+                'blue': 'with blue eyes',
+                'dark_red': 'with dark red eyes',
+                'dark_gray': 'with dark gray eyes',
+                'green': 'with green eyes',
+                'orange': 'with orange eyes',
+                'red': 'with red eyes',
+                'green_red': 'with green-red eyes',
+                'blue_yellow': 'with blue-yellow eyes',
+                'lazer': 'with laser eyes',
             };
             
-            return hairDescriptions[hair] || '';
+            return eyesDescriptions[eyes] || '';
 
         },
 
-        earrings: function () {
-            let earringDescriptions: Record<string, string> = {
-                'Silver_Hoop': 'wearing a silver hoop earring in the left ear',
-                'Silver_Stud': 'wearing a silver stud earring in the left ear',
-                'Gold_Hoop': 'wearing a gold hoop earring in the left ear',
-                'Gold_Stud': 'wearing a gold stud earring in the left ear',
-                'Silver_Stud_Alt_Side': 'wearing a silver stud earring in the right ear',
-                'Gold_Stud_Alt_Side': 'wearing a gold stud earring in the right ear',
-                'Diamond': 'wearing a diamond earring in the left ear',
+        face: function () {
+            let faceDescriptions: Record<string, string> = {
+                'normal': 'with a normal looking face',
+                'mask': 'wearing a mask',
+                'ninja': 'wearing a ninja mask',
+                'vizor': 'wearing a visor over their face',
+                'bandages': 'wearing bandages over their face',
+                'skull': 'with a skeleton skull face',
+                'noun_glasses': 'wearing square shaped glasses',
             };
 
-            return earringDescriptions[earrings] || '';
+            return faceDescriptions[face] || '';
         },
 
-        eyewear: function () {
-            let eyewearDescriptions: Record<string, string> = {
-                'Small_Shades': 'wearing small sunglasses',
-                'Regular_Shades': 'wearing sunglasses',
-                'Classic_Shades': 'wearing classic sunglasses',
-                'Horned_Rim_Glasses': 'wearing horn-rimmed eyeglasses',
-                'Eye_Patch': 'wearing an eye patch',
-                'AR': 'wearing a futuristic sci-fi eyepiece',
-                'VR': 'wearing virtual reality goggles',
-                '3D_Glasses': 'wearing 3D glasses',
-                'None': 'with beady round eyes'
+        feet: function () {
+            let feetDescriptions: Record<string, string> = {
+                'normal': 'barefoot',
+                'rocket_boots': 'wearing rocket boots',
+                'hoverboard': 'riding a hoverboard',
+                'skateboard': 'riding a skateboard',
             };
 
-            return eyewearDescriptions[eyewear] || '';
+            return feetDescriptions[feet] || '';
         },
 
-        headwear: function () {
-            let headwearDescriptions: Record<string, string> = {
-                'Pink_Headband': 'wearing a pink and white headband',
-                'Black_Cap': 'wearing a black ball cap',
-                'Do_Rag': 'wearing a do-rag',
-                'Bandana': 'wearing a blue bandana',
-                'Blue_Headband': 'wearing a blue and white headband',
-                'Purple_Cap': 'wearing a purple ball cap',
-                'Green_Headband': 'wearing a green and white headband',
-                'Orange_Knit': 'wearing an orange knit toque hat',
-                'Degen_Cap': 'wearing a flat-brim ball cap',
-                'Olive_Knit': 'wearing an olive-green knit toque hat',
-                'Police_Hat': 'wearing a police hat',
-                'Minty_Cap': 'wearing a mint-green ball cap',
-                'Farmer_Cap': 'wearing a farmer cap',
-                'LM_Knit': 'wearing a grey knit cap',
-                'Rancher_Hat': 'wearing a rancher hat',
-                'Cowboy_Hat': 'wearing a cowboy hat',
-                'Beanie': 'wearing a beanie',
-                'Top_Hat': 'wearing a top hat',
-                'Powdered_Wig': 'wearing a 18th century european-style powdered wig',
-                'Wizard_Cap': 'wearing a tall blue wizard cap',
-                'Devil': 'wearing devil horns',
-                'Fabulous_Cowboy_Hat': 'wearing a fancy pink cowboy hat',
-                'Angel': 'wearing an angel halo',
-                'Swamp_Ears': 'wearing Shrek ears'
+        hands: function () {
+            let handsDescriptions: Record<string, string> = {
+                'normal': 'having normal looking hands',
+                'cast_right': 'right hand shadowed',
+                'cast_left': 'left hand shadowed',
+                'kite': 'flying a kite',
             };
 
-            return headwearDescriptions[headwear] || '';
+            return handsDescriptions[hands] || '';
         },
 
 
-        headphones: function () {
-            let headphonesDescriptions: Record<string, string> = {
-                'Gray': 'wearing gray headphones',
-                'Red': 'wearing red headphones',
-                'Blue': 'wearing blue headphones',
-                'White': 'wearing white headphones',
-                'Striped': 'wearing striped headphones'
+        chest: function () {
+            let chestDescriptions: Record<string, string> = {
+                'orangered': 'and an orangered chest',
+                'light_gray': 'and a light gray colored chest',
+                'green': 'and a green colored chest',
+                'aqua': 'and an aqua colored chest',
+                'purple': 'and a purple colored chest',
+                'blue': 'and a blue colored chest',
+                'pink': 'and a pink colored chest',
+                'light_blue': 'and a light blue colored chest',
+                'gray': 'and a gray colored chest',
+                'yellow': 'and a yellow colored chest',
+                'orange': 'and an orange colored chest'
             };
 
-            return headphonesDescriptions[headphones] || '';
+            return chestDescriptions[chest] || '';
         },
 
+        head: function () {
+            let headDescriptions: Record<string, string> = {
+                'none': ' ',
+                'cap_backwards': 'wearing a backwards cap',
+                'two_tone_cap_backwards': 'wearing a two tone colored backwards cap',
+                'cap_forwards': 'wearing a forward facing cap',
+                'silly_yellow': 'wearing a silly yellow hat',
+                'crown': 'wearing a crown',
+                'chef': 'wearing a chef hat',
+                'bandana': 'wearing a bandana',
+                'long_peak_cap_forwards': 'wearing a long billed forward facing cap',
+                'headphones': 'wearing headphones',
+                'mouse_ears': 'wearing mouse ears'
+            
+            };
+
+            return headDescriptions[head] || '';
+        },
+
+        spikes: function () {
+            let spikesDescriptions: Record<string, string> = {
+                'light_gray': 'with light gray spikes',
+                'dark_gray': 'with dark gray spikes',
+                'green': 'with green spikes',
+                'pink': 'with pink spikes',
+                'orange': 'with orange spikes',
+                'white': 'with white spikes',
+                'purple': 'with purple spikes',
+                'teal': 'with teal spikes',
+                'yellow': 'with yellow spikes',
+                'burnt_orange': 'with burnt-orange colored spikes',
+                'blue': 'with blue spikes',
+                'red': 'with red spikes',
+                'multicolor': 'with multicolored spikes',
+                'none': 'without spikes'
+                };
+
+            return spikesDescriptions[spikes] || '';
+        },
 
         // build up the physical description string with the different parts
-        // special types zombie, ape, alien are different from commons
         getDescription: function () {
             let descriptionParts = [
-                this.type(),
-                isSpecialType ? "" : this.eyewear(),
-                this.headwear(),
-                isSpecialType ? "" : this.mouth(),
-                // hair === "None" ? "bald" : this.hair(),
-                this.hair(),
-                this.smoke(),
-                this.earrings(),
-                this.headphones()
+                this.feet(),
+                this.hands(),
+                this.body(),
+                this.eyes(),
+                this.background(),
+                this.face(),
+                this.chest(),
+                this.head(),
+                this.spikes()
             ];
 
 
@@ -216,7 +225,7 @@ function describe_traits(larmf: Larmf): string {
     };
 
     // call the fx and return the whole description
-    return `A simply-drawn cartoonish short, stumpy larva character ${phrases.getDescription.call(phrases)}`;
+    return `A tiny dinosaur ${phrases.getDescription.call(phrases)}`;
 
 }
 
